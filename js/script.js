1c8ee0a6e9664db02d0f45d601f78dc8fe450806
samuelebumbello <samuelebumbello17@gmail.com>
2023-04-15 19:37:16 +0200

    [add] array object containing tasks

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 const {createApp} = Vue;
 
 createApp({
-    data()[
-        
-    ],
+    data(){
+        return{
+            tasks: [
+                {
+                    text: 'Andare in palestra',
+                    done: false
+                },
+                {
+                    text: 'Andare a fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Prenotare visita radiologa',
+                    done: false
+                },
+            ],
+        }
+    },
 }).mount("#app")
